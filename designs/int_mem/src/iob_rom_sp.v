3022277f65e7d5c9b474951b46a27c93e649be63
PMiranda <pmiranda.iobundle@gmail.com>
2023-01-04 14:42:43 +0000

    feat(int_mem): iob-soc int_mem example - test with: ``` make mount ./flow.tcl -design int_mem ```

---
+++ designs/int_mem/src/iob_rom_sp.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+`include "defines.vh"
+
+module iob_rom_sp
+  #(
+    parameter DATA_W = 8,
+    parameter ADDR_W = 10,
+    parameter HEXFILE = "none"
+	)
+   (
+    input                    clk,
+    input                    r_en,
+    input [ADDR_W-1:0]       addr,
+    output [DATA_W-1:0]      r_data
+    );
+   
+    `include "boot_case.vh"
+   
+endmodule
